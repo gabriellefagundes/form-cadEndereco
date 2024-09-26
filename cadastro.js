8128808b7fa4b3fb3d66b41242dc53e3e01cb360
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-09-26 12:05:49 -0300

    edito funcionalidades no javascript, consequentemente tive que alterar css e html.

--- cadastro.js
+++ cadastro.js
@@ -1,27 +1,44 @@
-'use strict';//Modo restrito
-// Este modo faz com o que JavaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação
-/* Consumo de API - https://viacep.com.br/*/
+'use strict'; // Modo restrito
+// Este modo faz com que o JavaScript opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação
+/* Consumo de API - https://viacep.com.br/ */
  
 // Função para limpar formulário
-const LimparFormulario = ()=>{
-   document.getElementById('CEP').value = '';
-   document.getElementById('Logradouro').value = '';
-   document.getElementById('Bairro').value = '';
-   document.getElementById('UF').value = '';
-   
+const limparFormulario = () =>{
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('localidade').value = '';
+    document.getElementById('estado').value = '';
 }
-// Função para validar apenas números
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
  
-// lenght é uma propriedade que verifica a quantidade de caracteres dentro do argumento cep
-const cepValido = (cep) => cep.lenght == 8 && eNumero(cep);
- 
-// Função para preencher  formulário como campos API
+// Função para preencher formulário como campos da API
 const preencherFormulario = (endereco) =>{
     document.getElementById('logradouro').value = endereco.logradouro;
-    document.getElementById('Bairro').value = endereco.Bairro;
-    document.getElementById('UF').value = endereco.UF;
-   
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
 }
  
-//Função de consumo de API ViaCEP
+// cria regra de expressão regular (Regex) para testar valor informado pelo usuário
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+// Função de consumo de API ViaCEP
+const pesquisarCep = async() =>{
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+ 
+    if(cepValido(cep.value)){
+        const dados  = await fetch(url);
+        const addres = await dados.json();
+ 
+        if(addres.hasOwnProperty('erro')){
+            alert('CEP Não encontrado');  
+        }else{
+            preencherFormulario(addres);
+        }
+    }else{
+        alert("CEP Incorreto!");
+    }
+}
+ 
+// Adiciona escutador para executar consumo de API da ViaCEP
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
